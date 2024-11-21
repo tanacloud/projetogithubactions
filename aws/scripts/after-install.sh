cae28dc3bddd8888083cfd3e90856aeba91b5393
tanacloud <tanacloudbrasil@gmail.com>
2024-11-21 08:32:23 -0300

    change bucket s3 name

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack2-webappdeploymentbucket-mkkwmlnjuf5v/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://devopsstack-webappdeploymentbucket-nqjfqzqwwxmc/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
